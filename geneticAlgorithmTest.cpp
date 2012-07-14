5be9aaa378a7ce931389550f85034388cd304ca3
Will Oursler <woursler@gmail.com>
2012-07-14 18:54:42 -0400

    replaces tabs

--- geneticAlgorithmTest.cpp
+++ geneticAlgorithmTest.cpp
@@ -3,23 +3,23 @@
 #include "geneticAlgorithm.h"
 
 float getRandomCreature() {
-	return (float)rand()/(float)RAND_MAX;
+    return (float)rand()/(float)RAND_MAX;
 }
 
 float breed( float mom, float pop ) {
-	return ( mom + pop ) / 2 + (float)rand()/(float)RAND_MAX - 0.5;
+    return ( mom + pop ) / 2 + (float)rand()/(float)RAND_MAX - 0.5;
 }
 
 float fitness( float bob ) {
-	return bob;
+    return bob;
 }
 
 int main ()
 {
-	srand ( time(0) );
-	GeneticAlgorithm< float > floatBreeder( fitness, breed, getRandomCreature );
-	for( int i=0; i < 100; i++ ) {
-		std::cout<< floatBreeder.incrementGeneration() << "\n";
-	}
-	return 0;
+    srand ( time(0) );
+    GeneticAlgorithm< float > floatBreeder( fitness, breed, getRandomCreature );
+    for( int i=0; i < 100; i++ ) {
+        std::cout<< floatBreeder.incrementGeneration() << "\n";
+    }
+    return 0;
 }
