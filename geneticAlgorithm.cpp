0fd07b8ec3387e8159009cab09509424559ed907
Will Oursler <woursler@gmail.com>
2012-07-14 18:30:32 -0400

    Adds C++ implementation of Genetic Algorithms. Tested with float example, no known bugs.

---
+++ geneticAlgorithm.cpp
@@ -0,0 +1,62 @@
+//Picks an index of weights. A given index i is returned with probabilty p = weights[ i ] / sum( weights )
+int weightedChoice( std::vector< float > weights ) {
+
+	float sumOfWeights = 0;
+	for(
+		std::vector<float>::iterator j=weights.begin();
+		j!=weights.end();
+		++j
+	) {
+	    sumOfWeights += *j;
+	}
+	
+	float location =  sumOfWeights*(float)rand()/(float)RAND_MAX;
+
+	int index = 0;
+	for(
+		std::vector<float>::iterator j=weights.begin();
+		location > 0;
+		++j
+	) {
+	    location -= *j;
+	    index++;
+	}
+
+	return index - 1;
+}
+
+template <class Creature>
+GeneticAlgorithm<Creature>::GeneticAlgorithm( float (*fitness)( Creature ), Creature (*breed)( Creature, Creature ),  Creature (*getRandomCreature)() ) {
+	this->fitness = fitness;
+	this->breed = breed;
+	this->getRandomCreature = getRandomCreature;
+
+	this->populationSize = 1000;
+	this->population = new Creature[ this->populationSize ]();
+	for( int index = 0; index < this->populationSize; index ++ ) {
+		this->population[ index ] = (*(this->getRandomCreature))();
+	}
+}
+
+template <class Creature>
+float GeneticAlgorithm<Creature>::incrementGeneration() {
+	std::vector<float> fitnesses;
+	float sumFitness = 0;
+	for( int index = 0; index < this->populationSize; index ++ ) {
+		float fitness = (*(this->fitness))( this->population[ index ] );
+		fitnesses.push_back( fitness );
+		sumFitness += fitness;
+	}
+
+	Creature* newPopulation = new Creature[ this->populationSize ]();
+	for( int index = 0; index < this->populationSize; index ++ ) {
+		Creature mom = this->population[ weightedChoice( fitnesses )];
+		Creature pop = this->population[ weightedChoice( fitnesses )];
+		newPopulation[ index ] = (*(this->breed))( mom, pop );
+	}
+
+	this->fossilRecord.push_back( this->population );
+	this->population = newPopulation;
+
+	return sumFitness / this->populationSize;
+}
---
+++ geneticAlgorithmTest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "geneticAlgorithm.h"
+
+float getRandomCreature() {
+	return (float)rand()/(float)RAND_MAX;
+}
+
+float breed( float mom, float pop ) {
+	return ( mom + pop ) / 2 + (float)rand()/(float)RAND_MAX - 0.5;
+}
+
+float fitness( float bob ) {
+	return bob;
+}
+
+int main ()
+{
+	srand ( time(0) );
+	GeneticAlgorithm< float > floatBreeder( fitness, breed, getRandomCreature );
+	for( int i=0; i < 100; i++ ) {
+		std::cout<< floatBreeder.incrementGeneration() << "\n";
+	}
+	return 0;
+}
