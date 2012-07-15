5f363de654556f2e3098c2f4a76cf2d114db7a8b
Gurtej Kanwar <gurtej@mit.edu>
2012-07-15 18:08:36 -0400

    Implements basic point to point searching, but it's still partially broken

--- searchTest.cpp
+++ searchTest.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 #include "search.h"
-
-#include <cmath>
+#include "dijkstraSearch.h"
+#include "aStarSearch.h"
+#include "mine.h"
 
 template <class T>
 void print( std::vector< T > v ) {
@@ -17,6 +19,7 @@ void print( std::vector< T > v ) {
 	std::cout<<"\n";
 };
 
+/*
 namespace intSearch {
 	bool equals( int node1, int node2 ) {
 		return node1 == node2;
@@ -35,6 +38,7 @@ namespace intSearch {
 		return std::abs( node - goal );
 	}
 }
+*/
 
 /*
 obstacles = set()
@@ -68,9 +72,54 @@ def isGoal( point ):
 
 print dynamicSearch( (20,20), isGoal, neighbors, manhattanDistance )
 */
+namespace mineStateSearch {
+    // Just check if the robot is in the right place
+    bool equals(MineState* node1, MineState* node2) {
+	return (node1->getRobot() == node2->getRobot());
+    }
+
+    // Neighbors are all possible commands from the current node
+    std::vector< Edge< MineState* > > neighbors(MineState* node) {
+	char command;
+	MineState* tempMineState;
+	std::vector< Edge< MineState* > > edgesOut;
+	for (int i = 0; i < NUM_STANDARD_COMMANDS; i++) {
+	    command = standardCommands[i];
+	    if (i != 'W') {
+		tempMineState = stepMineState(node, command);
+		if (
+		    tempMineState->getRobot() != node->getRobot() &&
+		    !(tempMineState->isDone() && tempMineState->getDoneType() == LOSE)
+		    )
+		{
+		    // We only want to add the node if it's not equivalent to waiting
+		    edgesOut.push_back(*(new Edge<MineState*>(tempMineState, 1)));
+		}
+	    }
+	    else {
+		tempMineState = stepMineState(node, command);
+		if (!(tempMineState->isDone() && tempMineState->getDoneType() == LOSE)) {
+		    edgesOut.push_back(*(new Edge<MineState*>(tempMineState, 1)));
+		}
+	    }
+	}
+
+	return edgesOut;
+    }
+
+    // Use euclidean distance as the heuristic
+    float heuristic (MineState* node, MineState* goal) {
+	std::pair<int, int> robot = node->getRobot();
+	std::pair<int, int> robotGoal = goal->getRobot();
+	int xDist = robot.first - robotGoal.first;
+	int yDist = robot.second - robotGoal.second;
+	return sqrt(xDist*xDist + yDist*yDist);
+    }
+}
 
 int main ()
 {
+/*
 	//Use A* to do a trivial search.
 	try {
 		print< int >( aStarSearch( 30, 0, intSearch::equals, intSearch::neighbors, intSearch::heuristic ) );
@@ -84,6 +133,41 @@ int main ()
 	} catch( int error ) {
 		std::cout<< "Dijkstra's int search example failed.\n";
 	}
+*/
 
-	return 0;
+    // Use A* to do a search from one point to another
+    MineState* initialMine = new NaiveMineState("\
+######\n\
+#...R#\n\
+#**..#\n\
+#. .L#\n\
+##\\\\##\n\
+#....#\n\
+#\\\\*.#\n\
+#..\\.#\n\
+######");
+    MineState* goalMine = new NaiveMineState("\
+######\n\
+#... #\n\
+#**..#\n\
+#. .L#\n\
+##\\\\##\n\
+#....#\n\
+#\\\\*.#\n\
+#R.\\.#\n\
+######");
+
+    try {
+	std::vector<MineState*> nodes = aStarSearch(initialMine, goalMine, mineStateSearch::equals, mineStateSearch::neighbors, mineStateSearch::heuristic);
+	std::cout << "aStarSearch success" << std::endl;
+	for (int i = 0; i < nodes.size(); i++) {
+	    MineState* node = nodes[i];
+	    std::cout << (*node) << std::endl;
+	}
+    }
+    catch (int error) {
+	std::cout << "aStarSearch failed: " << error << std::endl;
+    }
+
+    return 0;
 }
