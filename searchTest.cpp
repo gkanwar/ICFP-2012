350b6eb2fad07665163b3231f193b29b4bda4bb0
Will Oursler <woursler@gmail.com>
2012-07-14 23:04:00 -0400

    Cleaning.

--- searchTest.cpp
+++ searchTest.cpp
@@ -36,6 +36,39 @@ namespace intSearch {
 	}
 }
 
+/*
+obstacles = set()
+for i in range( -10, 100 ):
+	obstacles.add( ( i, 5 ) )
+	obstacles.add( ( -10, i ) )
+
+def neighbors( point ):
+	(x,y) = point
+
+	candidates = [
+		(x-1, y),
+		(x+1, y),
+		(x, y-1),
+		(x, y+1)
+	]
+
+	for candidate in candidates:
+		if candidate in obstacles:
+			candidates.remove( canidate )
+
+	return set( [ (point, 1) for point in candidates] )
+
+def manhattanDistance( point ):
+	(x,y) = point
+	return abs(x) + abs(y)
+
+def isGoal( point ):
+	(x,y) = point
+	return ( x == 0 ) and ( y == 0 )
+
+print dynamicSearch( (20,20), isGoal, neighbors, manhattanDistance )
+*/
+
 int main ()
 {
 	//Use A* to do a trivial search.
