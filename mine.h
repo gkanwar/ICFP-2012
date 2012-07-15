e92391d4c33f4c43332cd8b18e0c3b399c692225
Gurtej Kanwar <gurtej@mit.edu>
2012-07-14 20:15:49 -0400

    Fixes the header file syntax errors (mine.h)

--- mine.h
+++ mine.h
@@ -2,23 +2,47 @@
  * mine.h
  */
 
+#ifndef MINE_H_INCLUDED
+#define MINE_H_INCLUDED
+
+#include <string>
+
 // Various possible commands
 #define NUM_STANDARD_COMMANDS 5
 #define EXTRA_COMMANDS 2
 char standardCommands[NUM_STANDARD_COMMANDS] = {'U', 'D', 'L', 'R', 'W'};
 char extraCommands[EXTRA_COMMANDS] = {'A', 'S'};
 
+// This is a map representation that naively makes copies
+// of the Whole Damn Map on each state update.
 class NaiveMineState
 {
+private:
+    // Regular data
+    int height, width;
+    char** grid;
+    std::pair<int, int> robot;
+
+    // Meta data
+    int water;
+    int flooding;
+    int waterproof;
+    // TODO: Add in the new meta data
+
+    // State data
+    bool winState;
+    bool doneState;
+
+public:
     // Constructor
-    NaiveMineState(string mineText);
+    NaiveMineState(std::string mineText);
     // Array access
     char& operator()(int height, int width);
     // Getters and setters
-    char getElement(pair<int, int> loc);
-    char setElement(pair<int, int> loc, char value);
-    pair<int, int> getRobot();
-    void setRobot(pair<int, int> loc); // Warning: This will overwrite the element at the robot location
+    char getElement(std::pair<int, int> loc);
+    char setElement(std::pair<int, int> loc, char value);
+    std::pair<int, int> getRobot();
+    void setRobot(std::pair<int, int> loc); // Warning: This will overwrite the element at the robot location
     int getWidth();
     int getHeight();
     bool isWon();
@@ -38,8 +62,12 @@ void printMineState(MineStateType state);
 
 template<class MineStateType> class MineSimulator
 {
+private:
+    MineStateType mineState;
+
+public:
     // Constructor
-    MineSimulator(string mineText);
+    MineSimulator(std::string mineText);
     // Printing
     void printState(MineStateType state);
     // Control the simulator
@@ -47,3 +75,4 @@ template<class MineStateType> class MineSimulator
     void transduce(char* commands, int numCommands);
 };
 	
+#endif // MINE_H_INCLUDED
